ae3a608222f59c8d5a7c63e80314aecb4fdfbee1
hennekes1 <81237981+hennekes1@users.noreply.github.com>
2021-06-02 13:42:18 +0200

    Add files via upload

--- SPH_2.jl
+++ SPH_2.jl
@@ -3,7 +3,7 @@ import Plots
 using Plots
 using LinearAlgebra
 
-function W(r,h)
+function Wcub(r,h)
     sigma = 40 / (7*pi*h^2)
     q = 1/h * norm(r)
     if 0<=q<=0.5
@@ -15,7 +15,7 @@ function W(r,h)
     end
 end
 
-function gradW(r,h)
+function dWcub(r,h)
     sigma = 40 / (7*pi*h^2)
     p = r
     q = 1/h * norm(r)
@@ -29,11 +29,58 @@ function gradW(r,h)
     if p>=0
         W=W*(-1)
     end
-    "wx = W *x
-    wy = W * y"
     return W
 end
 
+function d2Wcub(r,h)
+    sigma = 40 / (7*pi*h^2)
+    q = 1/h * norm(r)
+    if 0<=q<=0.5
+        W= -sigma*(-36*q+12)
+    elseif 0.5<q<=1
+        W= sigma*(12*(1-q))
+    else
+        W= 0
+    end
+    return W
+end
+
+function Wpoly6(r,h)
+    sigma = 315/(64*pi*h^9)
+    q = norm(r)
+    if 0<=q<h
+        W = sigma*(h^2-q^2)^3
+    else
+        W=0
+    end
+    return W
+end
+
+function dWpoly6(r,h)
+    sigma =945/(32*pi*h^9)
+    p=r
+    q = norm(r)
+    if 0<=q<h
+        W = sigma*(-r)*(h^2-q^2)^2
+    else
+        W=0
+    end
+    if p>=0
+        W=W*(-1)
+    end
+    return W
+end
+
+function d2Wpoly6(r,h)
+    sigma = 945/(8*pi*h^9)
+    q = norm(r)
+    if 0<=q<h
+        W = sigma * (q^2 - 0.75(h^2-q^2))*(h^2-q^2)
+    else
+        W=0
+    end
+    return W
+end
 
 function Gitter()
     xsmall=collect(-2:4/(gittergroeße-1):2)
@@ -58,7 +105,7 @@ function Dichte(pos,m,h)
     for i in 1:length(rho)
         sum = 0
         for j in 1:size(pos,1)
-            sum = sum + m*W(pos[i,:]-pos[j,:],h)
+            sum = sum + m*Wcub(pos[i,:]-pos[j,:],h)
         end
         rho[i] = sum
     end
@@ -71,28 +118,35 @@ function Druck(rho,k,n)
     return P
 end
 
-function Beschleunigung(pos,v,m,h,k,n)
+function F_Pressure(pos,v,m,h,k,n)
     rho = Dichte(pos,m,h)
     P = Druck(rho,k,n)
-    a = zeros(partikelanzahl,2)
+    f_pres = zeros(partikelanzahl,1)
     for i in 1:partikelanzahl
         xi = pos[i,:]
-        for j in (i+1):partikelanzahl
+        for j in 1:partikelanzahl
             xj = pos[j,:]
-            "a[i,1] = a[i,1]-m*((P[i] / (rho[i].*rho[i]))+(P[j] / (rho[j].*rho[j])))*gradW(Abstand(xi,xj),h)
-            a[i,2] = a[i,2]-m*((P[i] / (rho[i].*rho[i]))+(P[j] / (rho[j].*rho[j])))*gradW(Abstand(xi,xj),h)"
+            f_pres[i] = f_pres[i]+m*((P[i] / (rho[i].*rho[i]))+(P[j] / (rho[j].*rho[j])))*dWcub(Abstand(xi,xj),h)
         end
     end
+    return f_pres
+end
 
-    "Dämpfung und externe Kräfte"
-    "a[:,1] = a[:,1] - lambda .* pos[:,1] - nu .* v[:,1]
-    a[:,2] = a[:,2] - lambda .* pos[:,2] - nu .* v[:,2]"
-    return a
+function F_Viscosity(pos,v,m,h,k,n,nu)
+    f_vis = zeros(partikelanzahl,1)
+    for i in 1:partikelanzahl
+        xi = pos[i,:]
+        for j in 1:partikelanzahl
+            xj = pos[j,:]
+            f_vis[i] = f_vis[i] + m*((v[j,1]-v[i,1])/rho[j])*d2Wcub(Abstand(xi,xj),h)
+        end
+    end
+    f_vis = f_vis .* (nu./rho)
+    return f_vis
 end
 
 m = 0.005
 h = 0.1
-sigma = 40 / (7*pi*h^2)
 gittergroeße= 20
 x,y,anfangspos,xsmall,ysmall = Gitter()
 partikelanzahl = length(x)
@@ -104,15 +158,15 @@ n = 1
 v = ones(partikelanzahl,2)
 v = v*2
 v[:,2] .= v[:,2] * 0.75
-"damping"
+"Viskositätskonstante"
 nu = 1
 "external force constant--Wird hier nicht berechnet!"
 lambda = 2.01
-dt = 0.04
-
+"Schwerkraftkonstante"
+g = [0 ;-9.81]
 "Zeitintegration"
-
-time_steps = 50
+dt = 0.04
+time_steps = 9
 x_werte = zeros(partikelanzahl,time_steps+1)
 y_werte = zeros(partikelanzahl,time_steps+1)
 pos = [x[:] y[:]]
@@ -123,10 +177,18 @@ for j in 1:time_steps
     global pos = [x[:] y[:]]
     global rho = Dichte(pos,m,h)
     global P = Druck(rho,k,n)
-    global a = Beschleunigung(pos,v,m,h,k,n)
+    global f_pres = F_Pressure(pos,v,m,h,k,n)
+    global f_vis = F_Viscosity(pos,v,m,h,k,n,nu)
     for i in 1:partikelanzahl
-        v[i,1] = v[i,1] + dt*a[i,1]
-        v[i,2] = v[i,2] + dt*a[i,2]
+        "a[i,1] = -f_pres[i,1] + f_vis[i,1]
+        a[i,2] = -f_pres[i,2] + f_vis[i,2]"
+        "v[i,1] = v[i,1] + dt*a
+        v[i,2] = v[i,2] + dt*f_pres[i,2]"
+
+        global delta_v = -f_pres .+ f_vis
+        v[i,1] = v[i,1] + dt*delta_v[i]
+        v[i,2] = v[i,2] + dt*delta_v[i]
+
         x[i] = x[i] + dt*v[i,1]
         y[i] = y[i] + dt*v[i,2]
 
@@ -162,7 +224,8 @@ y_werte[:,time_steps+1] = y[:]
 global pos = [x[:] y[:]]
 global rho = Dichte(pos,m,h)
 global P = Druck(rho,k,n)
-global a = Beschleunigung(pos,v,m,h,k,n)
+global f_pres = F_Pressure(pos,v,m,h,k,n)
+
 
 scatter(x_werte, y_werte,layout=(3,2))
 scatter(x_werte[:,time_steps+1], y_werte[:,time_steps+1])
