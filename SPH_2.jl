0baf48b13bfb8df6c2550cf4f30797f1d90763b9
hennekes1 <81237981+hennekes1@users.noreply.github.com>
2021-06-08 12:05:47 +0200

    Add files via upload

--- SPH_2.jl
+++ SPH_2.jl
@@ -48,7 +48,7 @@ end
 function Wpoly6(r,h)
     sigma = 315/(64*pi*h^9)
     q = norm(r)
-    if 0<=q<h
+    if 0<=q<=h
         W = sigma*(h^2-q^2)^3
     else
         W=0
@@ -82,6 +82,56 @@ function d2Wpoly6(r,h)
     return W
 end
 
+function Wspiky(r,h)
+    sigma = 15/(pi*h^6)
+    q = norm(r)
+    if 0<=q<h
+        W = sigma * (h-q)^3
+    else
+        W=0
+    end
+    return W
+end
+
+function dWspiky(r,h)
+    sigma = -45/(pi*h^6)
+    q = norm(r)
+    if 0<=q<=h
+        W = sigma * (h-q)^2 * r/q
+    else
+        W=0
+    end
+    if r.>=0
+        W = W*(-1)
+    end
+    if q<=0.000000001
+        W = 14.32394487824193
+    end
+    return W
+end
+
+function Wviscosity(r,h)
+    sigma = 15/(2*pi*h^3)
+    q = norm(r)
+    if 0<=q<=h
+        W = sigma * ((-q^3)/(2*h^3)+(q^2/h^2)+(h/2*q)-1)
+    else
+        W=0
+    end
+    return W
+end
+
+function d2Wviscosity(r,h)
+    sigma = 45/(pi*h^6)
+    q = norm(r)
+    if 0<=q<=h
+        W = sigma *(h-q)
+    else
+        W=0
+    end
+    return W
+end
+
 function Gitter()
     xsmall=collect(-2:4/(gittergroeße-1):2)
     ysmall=collect(0:1.5/(gittergroeße-1):1.5)
@@ -104,8 +154,10 @@ function Dichte(pos,m,h)
     rho = zeros(size(pos,1))
     for i in 1:length(rho)
         sum = 0
+        xi = pos[i,:]
         for j in 1:size(pos,1)
-            sum = sum + m*Wcub(pos[i,:]-pos[j,:],h)
+            xj = pos[j,:]
+            sum = sum + m*Wpoly6(Abstand(xi,xj),h)
         end
         rho[i] = sum
     end
@@ -126,7 +178,7 @@ function F_Pressure(pos,v,m,h,k,n)
         xi = pos[i,:]
         for j in 1:partikelanzahl
             xj = pos[j,:]
-            f_pres[i] = f_pres[i]+m*((P[i] / (rho[i].*rho[i]))+(P[j] / (rho[j].*rho[j])))*dWcub(Abstand(xi,xj),h)
+            f_pres[i] = f_pres[i]+m*((P[i] / (rho[i].*rho[i]))+(P[j] / (rho[j].*rho[j])))*dWspiky(Abstand(xi,xj),h)
         end
     end
     return f_pres
@@ -138,7 +190,7 @@ function F_Viscosity(pos,v,m,h,k,n,nu)
         xi = pos[i,:]
         for j in 1:partikelanzahl
             xj = pos[j,:]
-            f_vis[i] = f_vis[i] + m*((v[j,1]-v[i,1])/rho[j])*d2Wcub(Abstand(xi,xj),h)
+            f_vis[i] = f_vis[i] + m*((v[j,1]-v[i,1])/rho[j])*d2Wviscosity(Abstand(xi,xj),h)
         end
     end
     f_vis = f_vis .* (nu./rho)
@@ -157,7 +209,7 @@ n = 1
 "Anfangsgeschwindigkeiten der Partikel"
 v = ones(partikelanzahl,2)
 v = v*2
-v[:,2] .= v[:,2] * 0.75
+v[:,2] .= v[:,2] * 0
 "Viskositätskonstante"
 nu = 1
 "external force constant--Wird hier nicht berechnet!"
@@ -166,7 +218,7 @@ lambda = 2.01
 g = [0 ;-9.81]
 "Zeitintegration"
 dt = 0.04
-time_steps = 9
+time_steps = 10
 x_werte = zeros(partikelanzahl,time_steps+1)
 y_werte = zeros(partikelanzahl,time_steps+1)
 pos = [x[:] y[:]]
@@ -180,14 +232,10 @@ for j in 1:time_steps
     global f_pres = F_Pressure(pos,v,m,h,k,n)
     global f_vis = F_Viscosity(pos,v,m,h,k,n,nu)
     for i in 1:partikelanzahl
-        "a[i,1] = -f_pres[i,1] + f_vis[i,1]
-        a[i,2] = -f_pres[i,2] + f_vis[i,2]"
-        "v[i,1] = v[i,1] + dt*a
-        v[i,2] = v[i,2] + dt*f_pres[i,2]"
-
-        global delta_v = -f_pres .+ f_vis
+        global delta_v = -f_pres./rho
+         ".+ f_vis"
         v[i,1] = v[i,1] + dt*delta_v[i]
-        v[i,2] = v[i,2] + dt*delta_v[i]
+        "v[i,2] = v[i,2] + dt*delta_v[i]"
 
         x[i] = x[i] + dt*v[i,1]
         y[i] = y[i] + dt*v[i,2]
@@ -217,6 +265,14 @@ for j in 1:time_steps
             x[i] = 2*(-2) - x[i]
             v[i,1] = v[i,1] * (-1)
         end
+        if y[i] .> 1.5
+            y[i] = 2*1.5 - y[i]
+            v[i,2] = v[i,2] *(-1)
+        end
+        if y[i] .< 0
+            y[i] = 2*0 - y[i]
+            v[i,2] = v[i,2] *(-1)
+        end
     end
 end
 x_werte[:,time_steps+1] = x[:]
@@ -225,7 +281,6 @@ global pos = [x[:] y[:]]
 global rho = Dichte(pos,m,h)
 global P = Druck(rho,k,n)
 global f_pres = F_Pressure(pos,v,m,h,k,n)
-
 
 scatter(x_werte, y_werte,layout=(3,2))
 scatter(x_werte[:,time_steps+1], y_werte[:,time_steps+1])
